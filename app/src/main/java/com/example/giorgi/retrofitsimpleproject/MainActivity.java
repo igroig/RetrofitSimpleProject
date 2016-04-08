ab437bb9c0ef56251d30f2aecfa5c4b2b84da7ac
giorgi <komarovi>
2016-04-08 17:26:15 +0400

    onClickEdit

--- app/src/main/java/com/example/giorgi/retrofitsimpleproject/MainActivity.java
+++ app/src/main/java/com/example/giorgi/retrofitsimpleproject/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.giorgi.retrofitsimpleproject;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.example.giorgi.retrofitsimpleproject.connections.RetrofitAPI;
 import com.example.giorgi.retrofitsimpleproject.models.Repo;
@@ -24,16 +19,35 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG =  "aaa";
+    private static final String TAG = "aaa";
+    private RecyclerView rvRepos;
+
+    List<Repo> lRepo;
+    RVAdapter rvAdapter;
+
+    private final String BASE_URL = "https://api.github.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // make retrofit class
-       Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.github.com").addConverterFactory(GsonConverterFactory.create()).build();
-        RetrofitAPI api = retrofit.create(RetrofitAPI.class);
+        setupViews();
+
+
+
+        //set adapter
+        LinearLayoutManager llManager = new LinearLayoutManager(this);
+        rvRepos.setLayoutManager(llManager);
+        rvAdapter = new RVAdapter();
+        rvRepos.setAdapter(rvAdapter);
+
+//        // make retrofit class
+//        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+//        RetrofitAPI api = retrofit.create(RetrofitAPI.class);
+
+        RetrofitAPI api = StRetrApi.getInstance().getRetrofitApi();
+
 
         //https://api.github.com/users/octocat/repos
         final Call<List<Repo>> repos = api.listRepos("octocat");
@@ -44,18 +58,14 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "onResponse: " + response.body().size());
 
                 // first json object name
-                Log.d(TAG+TAG, "onResponce:" + response.body().get(0).getName().toString());
+                Log.d(TAG + TAG, "onResponce:" + response.body().get(0).getName().toString());
 
-                // all names
-                StringBuilder sb = new StringBuilder();
-                String names= "";
-                for(int i=0; i<response.body().size(); i++){
-                    names =  response.body().get(i).getName().toString();
-                    sb.append(names + "\n");
-                }
-                Toast.makeText(getBaseContext(),sb, Toast.LENGTH_LONG ).show();
-                Log.d(TAG + TAG + TAG, "onResponce:" + sb);
 
+                //recycler
+
+                lRepo = response.body();
+                rvAdapter.addAll(lRepo);
+                rvAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -65,7 +75,10 @@ public class MainActivity extends AppCompatActivity {
         });
         Log.e(TAG, "onCreate: ");
 
+    }
 
+    public void setupViews() {
+        rvRepos = (RecyclerView) findViewById(R.id.rvRepos);
     }
 
 }
